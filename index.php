39d9a4d310be9482e8ad4d08044e88e71dccf136
Peter <99065921@mydavinci.nl>
2021-02-15 10:59:58 +0100

    Part A done

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+    $num1 = mt_rand(1, 1000);
+    $num2 = mt_rand(1, 1000);
+
+    $multiply = $num1 * $num2;
+    $addition = $num1 + $num2;
+    $subtraction = $num1 - $num2;
+    $division = $num1 / $num2;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AO-B3-W1-L3-Rekenen</title>
+</head>
+<body>
+    <p>
+        <?php
+            echo $num1 . " * " . $num2 . " = ". $multiply ."<br>";
+            echo $num1 . " + " . $num2 . " = ". $addition ."<br>";
+            echo $num1 . " - " . $num2 . " = ". $subtraction ."<br>";
+            echo $num1 . " / " . $num2 . " = ". $division;
+        ?>
+    </p>
+    
+</body>
+</html>
